962d54fe2f9bc0b00f87bcf115c4ef0c7fe4e5bf
David07994415 <david.game.power@gmail.com>
2025-12-21 20:45:46 +0800

    git init

---
+++ WebApplication1/Components/LatestNewsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Components
+{
+	public class LatestNewsViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			// 模擬資料，實際可從資料庫或 API 取得
+			var news = new List<string>
+			{
+				"AI 技術最新突破",
+				"C# .NET Core 更新",
+				"雲端服務趨勢"
+			};
+
+			return View(news);
+		}
+	}
+}
---
+++ WebApplication1/ViewModels/StudentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.ViewModels
+{
+	public class StudentViewModel
+	{
+        public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
